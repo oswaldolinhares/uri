59a86f72f2e1a23ca6f01264031dc29b8b0bdb65
Oswaldo Linhares <oswaldolinhares@gmail.com>
2020-07-08 18:32:50 -0300

    Soluções Beginners: 1042

---
+++ BegQ1042.rb
@@ -0,0 +1,9 @@
+vetor_original = gets.split.map(&:to_i)
+vetor = vetor_original.sort
+puts vetor[0]
+puts vetor[1]
+puts vetor[2]
+puts
+puts vetor_original[0]
+puts vetor_original[1]
+puts vetor_original[2]
